c8b1a5a36a39f24f1896a40a7c962232af98582d
Jack Shunn <jack.shunn@gmail.com>
2025-09-03 16:17:39 -0600

    Setup eslint use a typescript config file

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,13 @@ const nextConfig: NextConfig = {
     typedEnv: true,
   },
   typedRoutes: true,
-  
+  eslint: {
+    // In this repo use `npm run lint` to run the full lint
+    // Vscode should show you lint errors as you type
+    // and pull request restrictions will run the full lint too
+    "ignoreDuringBuilds": true,
+  },
+
   // from https://env.t3.gg/docs/nextjs :
   // if you turn on `output: "standalone"`, you need to make sure to include `transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"]`
 };
