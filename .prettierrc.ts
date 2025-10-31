4c553535c3cb984fead82b334fe31fe7345b71cd
Jack Shunn <85843070+jackshunn@users.noreply.github.com>
2025-10-30 20:15:11 -0600

    clean up package.json file and add sql formatter

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -1,7 +1,22 @@
 import type { Config } from "prettier";
+import type { SqlFormatOptions } from "prettier-plugin-sql";
+
+const sqlOptions: SqlFormatOptions = {
+  formatter: "sql-formatter",
+
+  language: "postgresql",
+  keywordCase: "lower",
+  dataTypeCase: "lower",
+  functionCase: "upper",
+  identifierCase: "upper",
+  indentStyle: "standard",
+  linesBetweenQueries: 3,
+};
 
 const config: Config = {
   experimentalTernaries: true,
+  plugins: ["prettier-plugin-sql"],
+  ...sqlOptions,
 };
 
 export default config;
