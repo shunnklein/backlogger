e8be607909ac2eeed471c6c0d8a2791a5547e373
Jack Shunn <85843070+jackshunn@users.noreply.github.com>
2025-11-01 18:38:22 -0600

    Split prisma schema into multiple files for better organization. (#58)

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 import type { PrismaConfig } from "prisma";
 
 export default {
-  schema: path.join("prisma", "schema.prisma"),
+  schema: path.join("prisma", "schema"),
   migrations: {
     path: path.join("prisma", "migrations"),
   },
